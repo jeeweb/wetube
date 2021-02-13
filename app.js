b60971aa4ff3e46b44010e223bf0ee24046a57f2
jeeweb <52396673+jeeweb@users.noreply.github.com>
2021-02-13 22:43:04 +0900

    index.js -> app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+const app = express()
+
+const handleHome = (req, res) => res.send('Hello from home');
+
+const handleProfile = (req, res) => res.send('You are on my profile');
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev"));
+
+app.get("/", handleHome)
+
+app.get("/profile", handleProfile)
+
+export default app;
