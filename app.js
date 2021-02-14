a6806c67d443ca2f969f79b0b4ad61b9348c6e87
jeeweb <52396673+jeeweb@users.noreply.github.com>
2021-02-14 22:52:21 +0900

    routing, MVC

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
 const app = express()
-
-const handleHome = (req, res) => res.send('Hello from home');
-
-const handleProfile = (req, res) => res.send('You are on my profile');
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -15,8 +15,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.get("/", handleHome)
 
-app.get("/profile", handleProfile)
+app.use(routes.home, globalRouter)
+app.use(routes.users, userRouter)
+app.use(routes.videos, videoRouter)
 
 export default app;
