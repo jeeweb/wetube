a711fe4c28aa4bb5a5728455b7d779549c66315e
jiee <jee.vvvv@gmail.com>
2021-02-16 21:18:19 +0900

    common template with Pug

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,7 +1,6 @@
-export const home = (req, res) => res.render("home");
-export const search = (req, res) => res.send("Search");
-export const videos = (req, res) => res.send("Videos");
-export const upload = (req, res) => res.send("Upload");
-export const videoDetail = (req, res) => res.send("Video Detail");
-export const editVideo = (req, res) => res.send("Edit Video");
-export const deleteVideo = (req, res) => res.send("Delete Video");
+export const home = (req, res) => res.render("home", { pageTitle: "Home" });
+export const search = (req, res) => res.send("Search", { pageTitle: "Search" });
+export const upload = (req, res) => res.send("Upload", { pageTitle: "Upload" });
+export const videoDetail = (req, res) => res.send("Video Detail", { pageTitle: "Video Detail" });
+export const editVideo = (req, res) => res.send("Edit Video", { pageTitle: "Edit Video" });
+export const deleteVideo = (req, res) => res.send("Delete Video", { pageTitle: "Delete Video" });
