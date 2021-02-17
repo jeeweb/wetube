7592f24980ce36dfe3a316db7a5b861e831d6bed
jiee <jee.vvvv@gmail.com>
2021-02-17 17:36:30 +0900

    join, login

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,7 @@
-export const join = (req, res) => res.send("Join");
-export const login = (req, res) => res.send("Login");
-export const logout = (req, res) => res.send("Logout");
-export const users = (req, res) => res.send("Users");
-export const userDetail = (req, res) => res.send("User Detail");
-export const editProfile = (req, res) => res.send("Edit Profile");
-export const changePassword = (req, res) => res.send("Change Password");
+export const join = (req, res) => res.render("Join", { pageTitle: "Join" });
+export const login = (req, res) => res.render("Login", { pageTitle: "Login" });
+export const logout = (req, res) => res.render("Logout", { pageTitle: "Logout" });
+export const users = (req, res) => res.render("Users", { pageTitle: "Users" });
+export const userDetail = (req, res) => res.render("User Detail", { pageTitle: "User Detail" });
+export const editProfile = (req, res) => res.render("Edit Profile", { pageTitle: "Edit Profile" });
+export const changePassword = (req, res) => res.render("Change Password", { pageTitle: "Change Password" });
