f1c45b2d5f9d1ccf4b43c6cdb6005541ee3252f9
jiee <jee.vvvv@gmail.com>
2021-02-19 15:32:42 +0900

    router처리

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,33 @@
-export const join = (req, res) => res.render("join", { pageTitle: "Join" });
-export const login = (req, res) => res.render("login", { pageTitle: "Login" });
-export const logout = (req, res) => res.render("logout", { pageTitle: "Logout" });
+import routes from "../routes";
+
+export const getJoin = (req, res) => {
+	res.render("join", { pageTitle: "Join" })
+};
+export const postJoin = (req, res) => {
+	console.log(req.body);
+	const {
+		boy: { name, email, password, password2 }
+	} = req;
+	if (password != password2) {
+		res.status(400)		// 비밀번호 2개가 다를 경우 상태코드로 400을 내보냄
+	} else {
+		// To DO: Register User
+		// To DO: Log user in
+		res.redirect(routes.home)
+	}
+	res.render("join", { pageTitle: "Join" })
+}
+
+export const getLogin = (req, res) => res.render("login", { pageTitle: "Login" });
+export const postLogin = (req, res) => {
+	res.redirect(routes.home);
+};
+
+export const logout = (req, res) => {
+	// To Do: Process Log Out
+	res.redirect(routes.home)
+};
+
 export const users = (req, res) => res.render("users", { pageTitle: "Users" });
 export const userDetail = (req, res) => res.render("userDetail", { pageTitle: "User Detail" });
 export const editProfile = (req, res) => res.render("editProfile", { pageTitle: "Edit Profile" });
--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,14 +1,26 @@
 import { videos } from "../db"
+import routes from "../routes";
 
 export const home = (req, res) => res.render("home", { pageTitle: "Home", videos });
 export const search = (req, res) => {
 	// console.log(req);
 	console.log(req.query.term);
 	// const searchingBy = req.query.term;	//EC6 이전의 선언방식
-	const { query: { term: searchingBy }} = req;	// term = req.query.term 이라고 선언하는 것보다 더 좋은 방식
-	res.render("Search", { pageTitle: "Search", searchingBy });
+	const {
+		query: { term: searchingBy }
+	} = req;	// term = req.query.term 이라고 선언하는 것보다 더 좋은 방식
+	res.render("search", { pageTitle: "Search", searchingBy, videos });
 }
-export const upload = (req, res) => res.render("Upload", { pageTitle: "Upload" });
-export const videoDetail = (req, res) => res.render("Video Detail", { pageTitle: "Video Detail" });
-export const editVideo = (req, res) => res.render("Edit Video", { pageTitle: "Edit Video" });
-export const deleteVideo = (req, res) => res.render("Delete Video", { pageTitle: "Delete Video" });
+export const getUpload = (req, res) => res.render("upload", { pageTitle: "Upload" });
+export const postUpload = (req, res) => {
+	const {
+		body: { file, title, description }
+	} = req;
+	// To do: Upload and save video
+	res.redirect(routes.videoDetail(324393))
+};
+
+
+export const videoDetail = (req, res) => res.render("videoDetail", { pageTitle: "Video Detail" });
+export const editVideo = (req, res) => res.render("editVideo", { pageTitle: "Edit Video" });
+export const deleteVideo = (req, res) => res.render("deleteVideo", { pageTitle: "Delete Video" });
