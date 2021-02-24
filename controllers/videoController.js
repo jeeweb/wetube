00c538336eaca3e30d4bd3a41ad763931422b5fb
jiee <jee.vvvv@gmail.com>
2021-02-24 23:58:51 +0900

    delete Video, installing ESLint

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -3,7 +3,7 @@ import Video from "../models/Video";
 
 export const home = async (req, res) => {
 	try{
-		const videos = await Video.find({}); 
+		const videos = await Video.find({}).sort({ _id: -1 }); 
 		res.render("home", { pageTitle: "Home", videos });
 	} catch(error){
 		console.log(error);
@@ -42,7 +42,7 @@ export const videoDetail = async (req, res) => {
 	try{
 		const video = await Video.findById(id);
 		console.log(video);
-		res.render("videoDetail", { pageTitle: "Video Detail", video });
+		res.render("videoDetail", { pageTitle: video.title, video });
 	} catch(error) {
 		// console.log(error);
 		res.redirect(routes.home);
@@ -66,11 +66,19 @@ export const postEditVideo = async (req, res) => {	// video를 업데이트
 		body: {title, description}
 	} = req;
 	try{
-		await Video.findOneAndUpdate({ id }, { title, description })
+		await Video.findOneAndUpdate({ _id: id }, { title, description })
 		res.redirect(routes.videoDetail(id));
 	} catch(error){
 		res.redirect(routes.home);
 	}
 };
 
-export const deleteVideo = (req, res) => res.render("deleteVideo", { pageTitle: "Delete Video" });
+export const deleteVideo = async (req, res) => {
+	const {
+		params: {id}
+	} = req;
+	try{
+		await Video.findOneAndRemove({ _id: id});
+	} catch(error){}
+	res.redirect(routes.home)
+};
