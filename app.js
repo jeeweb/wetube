479febfa837c6fedfc5189e5f811de10fbd3915f
jiee <jee.vvvv@gmail.com>
2021-03-01 23:30:14 +0900

    css연결,  installing babel/polyfill

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.use(function(req, res, next) {
 });
 app.set("view engine", "pug")
 app.use("/uploads", express.static("uploads"))
+app.use("/static", express.static("static"))
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
