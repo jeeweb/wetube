41b9f3029a06c8e434868cbc152b75c9bb4d81b9
jiee <jee.vvvv@gmail.com>
2021-05-26 23:08:16 +0900

    middleware 학습

--- src/server.js
+++ src/server.js
@@ -3,15 +3,34 @@ import express from "express";
 const PORT = 4000;
 const app = express();
 
+const logger = (req, res, next) => {
+	console.log(`${req.method} ${req.url}`);
+	next();
+}
+const privateMiddleware = (req, res, next) => {
+	const url = req.url;
+	if(url === "/protected"){
+		return res.send("<h1>Not Allowed</h1>");
+	}
+	console.log("Allowed, you may continue.")
+	next();
+}
+
 const handleHome = (req, res) => {
 	return res.send("this is home")
 }
 const handleLogin = (req, res) => {
 	return res.send("Login here.")
 }
+const handleProtected = (req, res) => {
+	return res.send("Welcome to the private lounge.")
+}
 
+app.use(logger);
+app.use(privateMiddleware);
 app.get("/", handleHome)
 app.get("/login", handleLogin)
+app.get("/protected", handleProtected)
 
 const handleListening = () => console.log(`Server Listening on port https://localhost:${PORT} 🚀`)
 
