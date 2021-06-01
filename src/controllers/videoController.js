9d5e773f6a331207f292172b170834d09e32c945
jiee <jee.vvvv@gmail.com>
2021-06-01 18:36:18 +0900

    user로그인 유무에 따른 header 메뉴 변경

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,9 @@
-export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
+const fakeUser = {
+	username: "Nico",
+	loggedIn: false,
+}
+
+export const trending = (req, res) => res.render("home", { pageTitle: "Home", fakeUser });
 export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
 export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
 export const search = (req, res) => res.send("Search");
