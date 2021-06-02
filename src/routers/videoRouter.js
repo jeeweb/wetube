bda724b86bcb02746adf584ea1ad3657540bb662
jiee <jee.vvvv@gmail.com>
2021-06-03 05:30:22 +0900

    /watch 페이지, /edit페이지 작업

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,11 +1,13 @@
 import express from "express";
-import { see, edit, upload, deleteVideo } from "../controllers/videoController";
+import { watch, getEdit, postEdit, upload, deleteVideo } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
 videoRouter.get("/upload", upload)
-videoRouter.get("/:id(\\d+)", see)
-videoRouter.get("/:id(\\d+)/edit", edit)
+videoRouter.get("/:id(\\d+)", watch)
+videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+//videoRouter.get("/:id(\\d+)/edit", getEdit)
+//videoRouter.post("/:id(\\d+)/edit", postEdit)
 videoRouter.get("/:id(\\d+)/delete", deleteVideo)
 
 export default videoRouter;
