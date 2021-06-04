7a357b0cbaf4411dff30e7a4145f401a91f39b15
jiee <jee.vvvv@gmail.com>
2021-06-04 21:01:09 +0900

    upload 기능 추가

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -46,3 +46,21 @@ export const postEdit = (req, res) => {
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => res.send("Delete Video");
+
+export const getUpload = (req, res) => {
+	return res.render("upload", {pageTitle: "Upload Video"});
+}
+export const postUpload = (req, res) => {
+	// console.log(req.body)
+	const { title } = req.body;
+	const newVideo = {
+		title,
+		rating: 0,
+		comments: 0,
+		createdAt: "just now",
+		views: 0,
+		id: videos.length + 1
+	};
+	videos.push(newVideo);
+	return res.redirect("/")
+}
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,13 +1,11 @@
 import express from "express";
-import { watch, getEdit, postEdit, upload, deleteVideo } from "../controllers/videoController";
+import { watch, getEdit, postEdit, getUpload, postUpload, deleteVideo } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/upload", upload)
+videoRouter.router("/upload").get(getUpload).post(postUpload)
 videoRouter.get("/:id(\\d+)", watch)
 videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
-//videoRouter.get("/:id(\\d+)/edit", getEdit)
-//videoRouter.post("/:id(\\d+)/edit", postEdit)
 videoRouter.get("/:id(\\d+)/delete", deleteVideo)
 
 export default videoRouter;
