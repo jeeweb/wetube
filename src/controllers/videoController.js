844edef7d6d560ee11ad615a952025924ed7b9e5
jiee <jee.vvvv@gmail.com>
2021-06-11 02:33:37 +0900

    search (+regex)

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -9,7 +9,7 @@ export const home = async (req, res) => {
 		return res.render("home", { pageTitle: "Home", videos });
 	});
 	*/
-  const videos = await Video.find({});
+  const videos = await Video.find({}).sort({createdAt: "desc"});
   return res.render("home", { pageTitle: "Home", videos });
 };
 export const watch = async (req, res) => {
@@ -75,4 +75,18 @@ export const deleteVideo = async (req, res) => {
 	const { id } = req.params;
 	await Video.findByIdAndDelete(id);
 	return res.redirect("/")
+}
+
+export const search = async (req, res) => {
+	const { keyword } = req.query;
+	let videos = [];
+	if (keyword) {
+		videos = await Video.find({
+			title: {
+				$regex: new RegExp(keyword, "i")
+			},
+		})
+		return res.render("search", { pageTitle: "Search", videos })
+	}
+	return res.render("search", { pageTitle: "Search"})
 }
