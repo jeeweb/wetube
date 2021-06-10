deaf5149d8264f2068fbd325f858ca7dc2d6229c
jiee <jee.vvvv@gmail.com>
2021-06-10 17:47:47 +0900

    added 404page, home link menu

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -14,7 +14,10 @@ export const home = async (req, res) => {
 export const watch = async (req, res) => {
   const { id } = req.params;
 	const video = await Video.findById(id);
-  return res.render("watch", { pageTitle: video.title, video });
+	if (video){
+		return res.render("watch", { pageTitle: video.title, video });
+	}
+	return res.render("404", { pageTitle: "Video not found."});
 };
 export const getEdit = (req, res) => {
   const { id } = req.params;
