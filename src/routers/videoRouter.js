b2e9411b240c79bbf4667fa47bfe7dd5802a25e6
jiee <jee.vvvv@gmail.com>
2021-06-11 01:38:43 +0900

    delete video

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -5,6 +5,7 @@ const videoRouter = express.Router();
 
 videoRouter.get("/:id([0-9a-f]{24})", watch)
 videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
+videoRouter.get("/:id([0-9a-f]{24})/delete", deleteVideo)
 videoRouter.route("/upload").get(getUpload).post(postUpload)
 
 export default videoRouter;
