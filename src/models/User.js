8698765a11777bf4ab2c484735d427b736c69e26
jiee <jee.vvvv@gmail.com>
2021-06-11 15:31:08 +0900

    join페이지작업, password hash (installed bcrypt)

---
+++ src/models/User.js
@@ -0,0 +1,32 @@
+import bcrypt from "bcrypt";
+import monoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+	email: {
+		type: String,
+		required: true,
+		unique: true
+	},
+	username: {
+		type: String,
+		required: true,
+		unique: true
+	},
+	password: {
+		type: String,
+		required: true
+	},
+	namd: {
+		type: String,
+		required: true
+	},
+	location: String
+})
+
+userSchema.pre("save", async function() {
+	this.password = await bcrypt.hash(this.password, 5)
+})
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
