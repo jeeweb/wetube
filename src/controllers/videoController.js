72c1c740b3fe6f4956582e5edfdf17618040effa
jiee <jee.vvvv@gmail.com>
2021-06-11 16:21:47 +0900

    error일때 status code 적용

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -24,7 +24,7 @@ export const getEdit = async (req, res) => {
   const { id } = req.params;
 	const video = await Video.findById(id);
 	if (!video){
-		return res.render("404", { pageTitle: "Video not found."});
+		return res.status(404).render("404", { pageTitle: "Video not found."});
 	}
   return res.render("edit", { pageTitle: `Edit ${video.title}`, video });
 };
@@ -33,7 +33,7 @@ export const postEdit = async (req, res) => {
 	const { title, description, hashtags } = req.body;
   const video = await Video.exists({_id: id})
   if (!video){
-		return res.render("404", { pageTitle: "Video not found."});
+		return res.status(404).render("404", { pageTitle: "Video not found."});
 	}
 	/*
 	video.title = title;
@@ -64,7 +64,7 @@ export const postUpload = async (req, res) => {
     });
     return res.redirect("/");
   } catch (error) {
-    return res.render("upload", {
+    return res.status(400).render("upload", {
       pageTitle: "Upload Video",
       errorMessage: error._message,
     });
