d2ce73ef48ead2f709a6982cdb0d6630de53804f
jeeweb <52396673+jeeweb@users.noreply.github.com>
2023-02-07 23:29:32 +0900

    route 설정 -> res.send()

--- src/server.js
+++ src/server.js
@@ -3,6 +3,17 @@ import express from "express";
 const PORT = 4004;
 const app = express();
 
+const handleHome = (req, res) => {
+  // return res.end();
+  return res.send("Home");
+};
+const handleLogin = (req, res) => {
+  return res.send("Login here");
+};
+
+app.get("/", handleHome);
+app.get("/login", handleLogin);
+
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
 
