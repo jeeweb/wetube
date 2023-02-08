46310ab9352be06bca172b273dd7e9aa444f3cc4
jeeweb <52396673+jeeweb@users.noreply.github.com>
2023-02-08 23:55:48 +0900

    middleware 연습

--- src/server.js
+++ src/server.js
@@ -1,18 +1,40 @@
 import express from "express";
+import res from "express/lib/response";
 
 const PORT = 4004;
 const app = express();
 
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
+
+const privateMiddleware = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") {
+    return res.send("<h1>Not Allowed</h1>");
+  }
+  console.log("Allowed, you may continue.");
+  next();
+};
+
 const handleHome = (req, res) => {
   // return res.end();
-  return res.send("Home");
+  return res.send("I love middlewares");
 };
 const handleLogin = (req, res) => {
   return res.send("Login here");
 };
+const handleProtected = (req, res) => {
+  return res.send("Welcome to the private lounge.");
+};
+
+app.use(logger);
+app.use(privateMiddleware);
 
 app.get("/", handleHome);
 app.get("/login", handleLogin);
+app.get("/protected", handleProtected);
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
