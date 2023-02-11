1b86a276f56bcbf79c3c76f32ff95f12833837da
jeeweb <52396673+jeeweb@users.noreply.github.com>
2023-02-11 23:47:20 +0900

    router 추가, URL Parameter

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,3 +1,12 @@
 export const trending = (req, res) => res.send("Home Page Videos");
-export const watch = (req, res) => res.send("Watch Video");
-export const edit = (req, res) => res.send("Edit Video");
+export const see = (req, res) => {
+  res.send(`Watch Video #${req.params.id}`);
+};
+export const edit = (req, res) => {
+  res.send("Edit Video");
+};
+export const search = (req, res) => res.send("Search");
+export const upload = (req, res) => res.send("Upload");
+export const deleteVideo = (req, res) => {
+  res.send("Delete Video");
+};
