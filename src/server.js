8b756bafb030e533edf8b461f7a92687afbccd59
jeeweb <52396673+jeeweb@users.noreply.github.com>
2023-02-11 11:27:55 +0900

    router 작성 및 분리

--- src/server.js
+++ src/server.js
@@ -1,24 +1,19 @@
 import express from "express";
 import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
-const PORT = 4004;
+const PORT = 4000;
 const app = express();
 const logger = morgan("dev");
-
-const home = (req, res) => {
-  console.log("I will respond.");
-  res.send("hello");
-};
-
-const login = (req, res) => {
-  return res.send("login");
-};
-
 app.use(logger);
-app.get("/", home);
-app.get("/login", login);
+
+app.use("/", globalRouter);
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
 
-app.listen(4004, handleListening);
+app.listen(PORT, handleListening);
