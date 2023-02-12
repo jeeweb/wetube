9a32874329052cc339d99c9910e37f188ed44644
jeeweb <52396673+jeeweb@users.noreply.github.com>
2023-02-12 11:56:58 +0900

    pug으로 view 화면 작업

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,10 +1,6 @@
-export const trending = (req, res) => res.send("Home Page Videos");
-export const see = (req, res) => {
-  res.send(`Watch Video #${req.params.id}`);
-};
-export const edit = (req, res) => {
-  res.send("Edit Video");
-};
+export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
+export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
+export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => {
--- src/server.js
+++ src/server.js
@@ -7,8 +7,10 @@ import videoRouter from "./routers/videoRouter";
 const PORT = 4000;
 const app = express();
 const logger = morgan("dev");
-app.use(logger);
 
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
+app.use(logger);
 app.use("/", globalRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
