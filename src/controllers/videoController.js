fdd4460bc73080b51007578cde3ebcf9438f8563
jeeweb <52396673+jeeweb@users.noreply.github.com>
2023-02-18 15:16:17 +0900

    upload 페이지 추가

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -48,7 +48,19 @@ export const postEdit = (req, res) => {
   videos[id].title = title;
   return res.redirect(`/videos/${id}`);
 };
-export const upload = (req, res) => res.send("Upload");
-export const deleteVideo = (req, res) => {
-  res.send("Delete Video");
+export const getUpload = (req, res) => {
+  res.render("upload", { pageTitle: "Upload Video" });
+};
+export const postUpload = (req, res) => {
+  const { title } = req.body;
+  const newVideo = {
+    title,
+    rating: 0,
+    comments: 0,
+    createdAt: "just now",
+    views: 0,
+    id: videos.length + 1,
+  };
+  videos.push(newVideo);
+  return res.redirect("/");
 };
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -3,12 +3,13 @@ import {
   watch,
   getEdit,
   postEdit,
-  upload,
+  getUpload,
+  postUpload,
 } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/upload", upload);
+videoRouter.route("/upload").get(getUpload).post(postUpload);
 videoRouter.get("/:id(\\d+)", watch);
 videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
 
