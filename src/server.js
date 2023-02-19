2f80d61b1b845553137a74c084f7843c7fe1dd7c
jeeweb <52396673+jeeweb@users.noreply.github.com>
2023-02-19 11:18:04 +0900

    server.js 코드 분리 (init.js)

--- src/server.js
+++ src/server.js
@@ -1,11 +1,9 @@
-import "./db";
 import express from "express";
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 
-const PORT = 4000;
 const app = express();
 const logger = morgan("dev");
 
@@ -17,7 +15,4 @@ app.use("/", globalRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
 
-const handleListening = () =>
-  console.log(`Server listening on port http://localhost:${PORT}`);
-
-app.listen(PORT, handleListening);
+export default app;
