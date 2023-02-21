ed57645fbf3ad52937edf4cb2a1f46b3e57668ed
jeeweb <52396673+jeeweb@users.noreply.github.com>
2023-02-21 23:54:09 +0900

    creating video

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -42,7 +42,18 @@ export const postEdit = (req, res) => {
 export const getUpload = (req, res) => {
   res.render("upload", { pageTitle: "Upload Video" });
 };
-export const postUpload = (req, res) => {
-  const { title } = req.body;
+export const postUpload = async (req, res) => {
+  const { title, description, hashtags } = req.body;
+  const video = new Video({
+    title,
+    description,
+    createdAt: Date.now(),
+    hashtags: hashtags.split(",").map((word) => `#${word}`),
+    meta: {
+      views: 0,
+      rating: 0,
+    },
+  });
+  await video.save();
   return res.redirect("/");
 };
