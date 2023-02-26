efaf1311581bc7325899b6d8ad3bc64c6dfd51aa
jeeweb <52396673+jeeweb@users.noreply.github.com>
2023-02-27 00:30:00 +0900

    save 전 middleware 작성

--- src/models/Video.js
+++ src/models/Video.js
@@ -11,5 +11,12 @@ const videoSchema = new mongoose.Schema({
   },
 });
 
+videoSchema.pre("save", async function () {
+  console.log("We are about to save: ", this);
+  this.hashtags = this.hashtags[0]
+    .split(",")
+    .map((word) => (word.startsWith("#") ? word : `#${word}`));
+});
+
 const Video = mongoose.model("Video", videoSchema);
 export default Video;
