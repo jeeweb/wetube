86593defd25cc12b73d7b69173afdce807139c5f
jeeweb <52396673+jeeweb@users.noreply.github.com>
2023-02-26 12:20:13 +0900

    getEdit 수정

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -29,15 +29,20 @@ export const watch = async (req, res) => {
   const { id } = req.params;
   const video = await Video.findById(id);
   //console.log(video);
-  if (video) {
-    return res.render("watch", { pageTitle: video.title, video });
+  if (!video) {
+    // video === null
+    return res.render("404", { pageTitle: "Video not found." });
   }
-  return res.render("404", { pageTitle: "Video not found." });
+  return res.render("watch", { pageTitle: video.title, video });
 };
 
-export const getEdit = (req, res) => {
+export const getEdit = async (req, res) => {
   const { id } = req.params;
-  return res.render("edit", { pageTitle: `Editing` });
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not found." });
+  }
+  return res.render("edit", { pageTitle: `Edit: ${video.title}`, video });
 };
 
 export const postEdit = (req, res) => {
