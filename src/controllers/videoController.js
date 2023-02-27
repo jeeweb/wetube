0107ce18b469302b7f2f5c3d0140f6058dcc1f20
jeeweb <52396673+jeeweb@users.noreply.github.com>
2023-02-27 23:54:47 +0900

    static으로 hashtags 정리하기

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -73,9 +73,7 @@ export const postEdit = async (req, res) => {
   await Video.findByIdAndUpdate(id, {
     title,
     description,
-    hashtags: hashtags
-      .split(",")
-      .map((word) => (word.startsWith("#") ? word : `#${word}`)),
+    hashtags: Video.formatHashtags(hashtags),
   });
   return res.redirect(`/videos/${id}`);
 };
@@ -90,7 +88,7 @@ export const postUpload = async (req, res) => {
     await Video.create({
       title,
       description,
-      hashtags,
+      hashtags: Video.formatHashtags(hashtags),
     });
     return res.redirect("/");
   } catch (error) {
--- src/models/Video.js
+++ src/models/Video.js
@@ -11,9 +11,16 @@ const videoSchema = new mongoose.Schema({
   },
 });
 
+/*
 videoSchema.pre("save", async function () {
   console.log("We are about to save: ", this);
   this.hashtags = this.hashtags[0]
+    .split(",")
+    .map((word) => (word.startsWith("#") ? word : `#${word}`));
+});
+*/
+videoSchema.static("formatHashtags", function (hashtags) {
+  return hashtags
     .split(",")
     .map((word) => (word.startsWith("#") ? word : `#${word}`));
 });
