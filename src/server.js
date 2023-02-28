940805f94eb4fe45fadd301717b1ecf18d097d46
jeeweb <52396673+jeeweb@users.noreply.github.com>
2023-02-28 23:55:09 +0900

    join 화면을 위한 pug, controller, router 작업

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import "./db";
 import "./models/Video";
 import express from "express";
 import morgan from "morgan";
-import globalRouter from "./routers/globalRouter";
+import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 
@@ -13,7 +13,7 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
-app.use("/", globalRouter);
+app.use("/", rootRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
 
