5f0bf2d7257cc2caadd1329373a90f070de4609d
jeeweb <52396673+jeeweb@users.noreply.github.com>
2023-03-05 09:46:38 +0900

    login

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/User";
+import bcrypt from "bcrypt";
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
 export const postJoin = async (req, res) => {
@@ -18,35 +19,49 @@ export const postJoin = async (req, res) => {
       errorMessage: "This username/email is already taken.",
     });
   }
-  /*
-  // username 중복체크
-  const usernameExists = await User.exists({ username });
-  if (usernameExists) {
-    return res.render("join", {
+
+  try {
+    await User.create({
+      name,
+      username,
+      email,
+      password,
+      location,
+    });
+    return res.redirect("/login");
+  } catch (error) {
+    return res.status(400).render("join", {
       pageTitle,
-      errorMessage: "This username is already taken.",
+      errorMessage: error._message,
     });
   }
-  // email 중복체크
-  const emailExists = await User.exists({ email });
-  if (emailExists) {
-    return res.render("join", {
-      pageTitle,
-      errorMessage: "This email is already taken.",
-    });
-  }
-  */
-  await User.create({
-    name,
-    username,
-    email,
-    password,
-    location,
-  });
-  return res.redirect("/login");
 };
+
+export const getLogin = (req, res) =>
+  res.render("login", { pageTitle: "Login" });
+export const postLogin = async (req, res) => {
+  const { username, password } = req.body;
+  const pageTitle = "Login";
+  // const exists = await User.exists({ username });  // user가 존재하는지 체크
+  const user = await User.findOne({ username }); // db에서 user 찾아오기
+  if (!user) {
+    return res.status(400).render("login", {
+      pageTitle,
+      errorMessage: "An account with this username does not exists.",
+    });
+  }
+  const ok = await bcrypt.compare(password, user.password);
+  if (!ok) {
+    return res.status(400).render("login", {
+      pageTitle,
+      errorMessage: "Wrong password.",
+    });
+  }
+  console.log("Log user in!");
+  return res.redirect("/");
+};
+
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Delete User");
-export const login = (req, res) => res.send("Login");
 export const logout = (req, res) => res.send("Log out");
 export const see = (req, res) => res.send("See User");
