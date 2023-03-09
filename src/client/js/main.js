7b1f60d6d73b4a00d2b10b4b8871667e7f618530
jeeweb <52396673+jeeweb@users.noreply.github.com>
2023-03-09 12:43:40 +0900

    webpack & scss

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,5 +1,3 @@
-const hello = async () => {
-  alert("hi!");
-  const x = await fetch("");
-};
-hello();
+import "../scss/styles.scss";
+
+console.log("hi");
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,19 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 
 module.exports = {
   entry: "./src/client/js/main.js",
   mode: "development",
+  watch: true,
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "css/styles.css",
+    }),
+  ],
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "assets", "js"),
+    filename: "js/main.js",
+    path: path.resolve(__dirname, "assets"),
+    clean: true,
   },
   module: {
     rules: [
@@ -17,6 +25,10 @@ module.exports = {
             presets: [["@babel/preset-env", { targets: "defaults" }]],
           },
         },
+      },
+      {
+        test: /\.scss/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
     ],
   },
