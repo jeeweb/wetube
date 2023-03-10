f17d3ef29fd407c1f8e49161c39cceb891179d18
jeeweb <52396673+jeeweb@users.noreply.github.com>
2023-03-10 12:52:31 +0900

    video control 아이콘으로 변경, keydown 이벤트 추가

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,11 +1,14 @@
 const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
+const playBtnIcon = playBtn.querySelector("i");
 const muteBtn = document.getElementById("mute");
+const muteBtnIcon = muteBtn.querySelector("i");
 const volumeRange = document.getElementById("volume");
 const currenTime = document.getElementById("currenTime");
 const totalTime = document.getElementById("totalTime");
 const timeline = document.getElementById("timeline");
 const fullScreenBtn = document.getElementById("fullScreen");
+const fullScreenIcon = fullScreenBtn.querySelector("i");
 const videoContainer = document.getElementById("videoContainer");
 const videoControls = document.getElementById("videoControls");
 
@@ -20,7 +23,7 @@ const handlePlayClick = (e) => {
   } else {
     video.pause();
   }
-  playBtn.innerText = video.paused ? "Play" : "Pause";
+  playBtnIcon.classList = video.paused ? "fas fa-play" : "fas fa-pause";
 };
 
 const handleMuteClick = (e) => {
@@ -29,7 +32,9 @@ const handleMuteClick = (e) => {
   } else {
     video.muted = true;
   }
-  muteBtn.innerText = video.muted ? "Unmute" : "Mute";
+  muteBtnIcon.classList = video.muted
+    ? "fas fa-volume-mute"
+    : "fas fa-volume-up";
   volumeRange.value = video.muted ? 0 : volumeValue;
 };
 
@@ -46,7 +51,7 @@ const handleVolumeChange = (event) => {
 };
 
 const formatTime = (seconds) =>
-  new Date(seconds * 1000).toISOString().substring(11, 19);
+  new Date(seconds * 1000).toISOString().substring(14, 19);
 
 const handleLoadedMetadata = () => {
   totalTime.innerText = formatTime(Math.floor(video.duration));
@@ -69,10 +74,26 @@ const handleFullscreen = () => {
   const fullscreen = document.fullscreenElement;
   if (fullscreen) {
     document.exitFullscreen();
-    fullScreenBtn.innerText = "Full Screen";
+    fullScreenIcon.classList = "fas fa-expand";
   } else {
     videoContainer.requestFullscreen();
-    fullScreenBtn.innerText = "Back to Browser";
+    fullScreenIcon.classList = "fas fa-compress";
+  }
+};
+
+const handleKeyDown = (event) => {
+  const fullscreen = document.fullscreenElement;
+  console.log(event.key, fullscreen);
+  if (event.key === " ") {
+    handlePlayClick();
+  } else if (event.key === "f") {
+    video.requestFullscreen();
+    fullScreenIcon.classList = "fas fa-compress";
+  } else if (event.key === "Escape") {
+    if (fullscreen !== null) {
+      document.exitFullscreen();
+      fullScreenIcon.classList = "fas fa-expand";
+    }
   }
 };
 
@@ -102,7 +123,8 @@ volumeRange.addEventListener("input", handleVolumeChange);
 video.addEventListener("timeupdate", handleTimeUpdate);
 video.addEventListener("mousemove", handleMouseMove);
 video.addEventListener("mouseleave", handleMouseLeave);
+document.addEventListener("keydown", handleKeyDown);
 timeline.addEventListener("input", handleTimelineChange);
 video.readyState
-  ? handleMetadata()
-  : video.addEventListener("loadedmetadata", handleLoadedMetadata);
+  ? handleLoadedMetadata()
+  : video.addEventListener("loadeddata", handleLoadedMetadata);
